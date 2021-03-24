c5ab5ebe3778718cc5167f4e4b7ad2e3994c969b
evanoralph <evanoralph@gmail.com>
2021-03-24 20:30:31 +0800

    Update User.js

--- src/user/User.js
+++ src/user/User.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Data from '../Data';
 import { hashPassword } from '../../lib/utils';
